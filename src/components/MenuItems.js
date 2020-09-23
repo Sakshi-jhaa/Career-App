c625cabbbc72b33c668acd0a9b83fcb3b04a563e
sakshi <jhasakshi1234@gmail.com>
2020-09-23 13:18:01 +0530

    FIrst commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: '8th-10th',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'After 12th',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'College Graduates',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Working Professionals',
+    path: '/development',
+    cName: 'dropdown-link'
+  }
+];
